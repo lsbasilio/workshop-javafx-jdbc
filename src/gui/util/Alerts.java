10bee7ec4ad6b8c547c6acf3e7b068ec82f795ac
lsbasilio <leandro_sanzi@yahoo.com.br>
2020-11-04 19:08:19 -0300

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,7 +1,11 @@
 package gui.util;
 
+import java.util.Optional;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import model.entities.Department;
 
 public class Alerts {
 	public static void showAlert(String title, String header, String content, AlertType type) {
@@ -11,4 +15,12 @@ public class Alerts {
 		alert.setContentText(content);
 		alert.show();
 	}
+	
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
+		}
 }
